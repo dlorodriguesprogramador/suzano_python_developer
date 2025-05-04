651ac1d6a1037e4a942a5c9851ff5ad5e659d120
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-04 12:49:49 -0300

    Create tipos_de_dados.py
    Estudos de tipos de dados

---
+++ tipos_de_dados.py
@@ -0,0 +1,45 @@
+# Tipos de dados
+
+# Por que usamos tipos?
+# Os tipos servem para definir as caracteristicas e comportamento de um valor (objeto) para o interpretador.
+# Por exemplo:
+# Com esse tipo eu sou capaz de realizar operações matemáticas.
+# Esse tipo para ser armazenado em memória irá consumir 24 bytes.
+
+# Tipos em Python
+# texto -> str
+# númerico -> int, float, complex
+# sequência -> list, tuple, range
+# mapa -> dict
+# coleção -> set, frozenset
+# booleano -> bool
+# binário -> bytes, bytearray, memoryview
+
+# Números inteiros
+# Números inteiros são representados pela classe int e possuem precisão ilimitada. São exemplos válidos de números inteiros: 1, 10, 100, -1, -10, -100...99001823
+
+# Números de ponto flutuante
+# Números de ponto flutuante são usados para representar os números racionais e sua implementação é feita pela classe float.
+# São exemplos válidos de números de ponto flutuante: 1.0, 10.0, 100.0, -1.0, -10.0, -100.0...99001823.0
+
+# Booleano
+# É usado para representar verdadeiro ou falso, e é implementado pela classe bool.
+# Em python o tipo booleano é uma subclasse de int, uma vez que qualquer número diferente de 0 representa verdadeiro e 0 representa falso.
+# São exemplos válidos de booleanos: True, False.
+
+# Strings
+# Strings ou cadeia de caracteres são usadas para representar valores alfanúmericos, em Python as strings são definidas utilizando a classe str.
+# São exemplos válidos de strings: "Olá", "123", "True", "10.0", "100"
+
+print("Olá, mundo!")
+
+print(10)
+print(11 + 10 + 1000)
+
+print(10.0)
+print(10.0 + 10.0)
+
+print(True)
+print(False)
+
+print("Python")

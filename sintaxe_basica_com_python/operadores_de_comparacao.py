6933580e633ea6dba984f53427fbc83d2f0893d2
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-05 10:41:01 -0300

    Create operadores_de_comparacao.py
    Usando operadores de comparação exemplos e exercícios.

---
+++ sintaxe_basica_com_python/operadores_de_comparacao.py
@@ -0,0 +1,59 @@
+# O que são?
+
+# São operadores utilizados para comparar dois valores.
+
+# Igualdade: ==
+# Exemplo: 
+a = 10
+b = 20
+print(a == b)
+
+# Diferença: !=
+# Exemplo: 
+a = 10
+b = 20
+print(a != b)
+
+# Maior que: >
+# Exemplo: 
+a = 10
+b = 20
+print(a > b)
+
+# Menor que: <
+# Exemplo: 
+a = 10
+b = 20
+print(a < b)
+
+# Maior ou igual a: >=
+# Exemplo: 
+a = 10
+b = 20
+print(a >= b)
+
+# Menor ou igual a: <=
+# Exemplo: 
+a = 10
+b = 20
+print(a <= b)
+
+# Exercício:
+
+a = 10
+b = 20
+c = 30
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)

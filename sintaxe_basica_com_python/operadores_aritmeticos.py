03e3d1ac1ec1f4682d53ed929eafcd440fe61ccc
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-05 10:08:10 -0300

    Create operadores_aritmeticos.py
    Exemplos e exercícios de operadores aritméticos.

---
+++ sintaxe_basica_com_python/operadores_aritmeticos.py
@@ -0,0 +1,102 @@
+# O que são?
+
+# Os operadores aritméticos executam operações matemáticas, como adição, subtração com operandos.
+
+# Adição: +
+# Exemplo:
+a = 10
+b = 20
+print(a + b)
+
+# Subtração: -
+# Exemplo: 
+a = 10
+b = 20
+print(a - b)
+
+# Multiplicação: *
+# Exemplo: 
+a = 10
+b = 20
+print(a * b)
+
+# Divisão: /
+# Exemplo: 
+a = 10
+b = 20
+print(a / b)
+
+# Divisão inteira: //
+# Exemplo: 
+a = 10
+b = 20
+print(a // b)
+
+# Módulo: %
+# Exemplo: 
+a = 10
+b = 20
+print(a % b)
+
+# Exponenciação: **
+# Exemplo: 
+a = 10
+b = 20
+print(a ** b)
+
+# Na matemática
+
+# Na matemática existe uma regra que indica quais operações devem ser execultadas preimeiro. 
+# Isso é útil pois ao analisar uma expressão, a depender da ordem das operações o valor pode ser diferente:
+
+# Exemplo: 
+a = 10
+b = 20
+c = 30
+print(a + b * c)
+
+# A definição indica a seguinte ordem como a correta:
+# 1. Parênteses
+# 2. Expoentes
+# 3. Multiplicação e divisão
+# 4. Adição e subtração
+
+# Exemplo: 
+a = 10
+b = 20
+c = 30
+print((a + b) * c)
+
+# Exercício:
+
+produto_1 = 30
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+x = (10 + 20) * 30
+print(x)
+y = 10 + (20 * 30)
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

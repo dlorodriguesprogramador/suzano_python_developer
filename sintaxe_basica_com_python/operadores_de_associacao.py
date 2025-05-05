abfe74e6b4969ff96854ade9eea56638e8441bd0
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-05 13:16:01 -0300

    Create operadores_de_associacao.py
    São operadores utilizados para verificar se um elemento está presente em uma sequência.

---
+++ sintaxe_basica_com_python/operadores_de_associacao.py
@@ -0,0 +1,25 @@
+# O que são?
+
+# São operadores utilizados para verificar se um elemento está presente em uma sequência.
+
+# Operador in:
+# Exemplo: 
+lista = [1, 2, 3, 4, 5]
+print(3 in lista)  # True, pois 3 está na lista
+
+# Operador not in:
+# Exemplo: 
+string = "Python"
+print("z" not in string)  # True, pois "z" não está na string
+
+# Exercício:
+
+lista_numeros = [10, 20, 30, 40, 50]
+string_palavras = "Python é uma linguagem de programação"
+
+print(20 in lista_numeros)  
+print(60 not in lista_numeros)  
+print("Python" in string_palavras)  
+print("Java" not in string_palavras)  
+print(30 in lista_numeros)  
+print("programação" in string_palavras)  

d722dbf0e9c7f3033cfc02a5a6d0f951b5deab9e
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-05 12:51:38 -0300

    Create operadores_de_atribuicao.py
    Operadores utilizado para definir valor inicial ou sobrescrever o valor de uma variável.

---
+++ sintaxe_basica_com_python/operadores_de_atribuicao.py
@@ -0,0 +1,77 @@
+# O que são?
+
+# São operadores utilizados para definir o valor inicial ou sobrescrever o valor de uma variável.
+
+# Atribuição: =
+# Exemplo: 
+a = 10
+print(a)
+
+# Atribuição de adição: +=
+# Exemplo: 
+a = 10
+a += 10
+print(a)
+
+# Atribuição de subtração: -=
+# Exemplo: 
+a = 10
+a -= 10
+print(a)
+
+# Atribuição de multiplicação: *=
+# Exemplo: 
+a = 10
+a *= 10
+print(a)
+
+# Atribuição de divisão: /=
+# Exemplo: 
+a = 10
+a /= 10
+print(a)
+
+# Atribuição de divisão inteira: //=
+# Exemplo: 
+a = 10
+a //= 10
+print(a)
+
+# Atribuição de resto da divisão: %=
+# Exemplo: 
+a = 10
+a %= 10
+print(a)
+
+# Atribuição de exponenciação: **=
+# Exemplo: 
+a = 10
+a **= 10
+print(a)
+
+# Exercício:
+
+a = 10
+b = 20
+c = 30
+
+a += b
+print(a)
+
+a -= c
+print(a)
+
+a *= c
+print(a)
+
+a /= b
+print(a)    
+
+a //= c
+print(a)
+
+a %= c
+print(a)
+
+a **= b
+print(a)

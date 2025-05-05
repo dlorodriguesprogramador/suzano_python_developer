82ae1ca3867e853ab08df2a4503f38a281a18378
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-05 13:13:13 -0300

    Create operadores_de_identidade.py
    São operadores utilizados para verificar se duas variáveis ocupam a mesma posição de memória.

---
+++ sintaxe_basica_com_python/operadores_de_identidade.py
@@ -0,0 +1,28 @@
+# O que são?
+
+# São operadores utilizados para verificar se duas variáveis ocupam a mesma posição de memória.
+
+# Operador is:
+# Exemplo: 
+a = 10
+b = 10
+print(a is b)
+
+# Operador is not:
+# Exemplo: 
+a = 10
+b = 20
+print(a is not b)
+
+# Exercício:
+
+a = 10
+b = 20
+c = 30  
+
+print(a is b)
+print(a is not b)
+print(a is c)
+print(a is not c)
+print(b is c)
+print(b is not c)

d84d3eaf59d2a667120a5f25b92db51f23b4c1f3
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-05 13:07:49 -0300

    Create operadores_logicos.py
    Operadores utilizados em conjunto com os operadores de comparação para criar expressões lógicas.

---
+++ sintaxe_basica_com_python/operadores_logicos.py
@@ -0,0 +1,35 @@
+# O que são?
+
+# São operadores utilizados em conjunto com os operadores de comparação para criar expressões lógicas. 
+# Quando uma expressão lógica é verdadeira, o resultado é True, e quando é falsa, o resultado é False.
+
+# Operador AND:
+# Exemplo: 
+a = 10
+b = 20
+c = 30
+print(a < b and b < c)
+
+# Operador OR:
+# Exemplo: 
+a = 10
+b = 20
+c = 30
+print(a < b or b > c)
+
+# Operador NOT:
+# Exemplo: 
+a = 10
+b = 20
+c = 30
+print(not a < b)
+
+# Exercício:
+
+a = 10
+b = 20
+c = 30 
+
+print(a < b and b < c)
+print(a < b or b > c)
+print(not a < b)

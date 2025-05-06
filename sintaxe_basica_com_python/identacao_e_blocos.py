7dbd5db9ea8a42d1cbd4d4099442e928035cfa33
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-06 10:47:50 -0300

    Create identacao_e_blocos.py
    Indentação e blocos em python.

---
+++ sintaxe_basica_com_python/identacao_e_blocos.py
@@ -0,0 +1,29 @@
+# O que é?
+
+# A identação é a prática de escrever o código com a mesma distância de espaço em branco na frente das linhas de código.
+
+# Por que isso é importante?
+
+# A identação é importante para que o código seja mais legível e fácil de entender.
+
+# Exemplo:
+
+x = int(input("Digite um número para x: "))
+
+if x > 10:
+    print("x é maior que 10")
+else:
+    print("x é menor que 10")
+
+
+# Exercício:
+
+# Crie um programa que receba um número e imprima se ele é par ou ímpar.
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
+

e1c6f11b8f7e72e82e42b7da5a994c534964e6b8
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-06 21:31:52 -0300

    Create fatiamento_de_string.py
    Fatiamento de strings é uma técnica utilizada para retornar substrings.

---
+++ sintaxe_basica_com_python/manipulando_strings_com_python/fatiamento_de_string.py
@@ -0,0 +1,20 @@
+# Introdução
+
+# Fatiamento de strings é uma técnica utilizada para retornar substrings 
+# (partes da string original), informando o inicio (start), o fim (end) e o passo (step).
+
+# Fatiamento
+
+nome = 'Guilherme Arthur de Carvalho'
+
+print(nome[0])
+
+print(nome[:9])
+
+print(nome[10:])
+
+print(nome[10:16])
+
+print(nome[10:16:2])
+
+print(nome[::-1])

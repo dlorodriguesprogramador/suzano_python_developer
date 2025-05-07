f0cc4d60754144ea64fe13a5536d8ff183f06a47
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-06 21:21:19 -0300

    Create conhecendo_metodo_uteis_da_classe_string.py
    Métodos úteis para manipular objetos do tipo string.

---
+++ sintaxe_basica_com_python/manipulando_strings_com_python/conhecendo_metodo_uteis_da_classe_string.py
@@ -0,0 +1,34 @@
+# Objetivo Geral
+
+# Conhecer métodos úteis para manipular objetos do tipo string, 
+# como interpolar valores de cariáveis e entender como funciona o fatiamento.
+
+# Maiúscula, minúscula e título.
+
+# Exemplo:
+
+nome = 'Guilherme'
+
+print(nome.upper())
+
+print(nome.lower())
+
+print(nome.title())
+
+# Eliminando espaços em branco.
+
+texto = '  Python  '
+
+print(texto.strip())
+
+print(texto.lstrip())
+
+print(texto.rstrip())
+
+# Junções e centralização.
+
+nome = 'Guilherme'
+
+print(nome.join(' '))
+
+print(nome.center(10, '#'))

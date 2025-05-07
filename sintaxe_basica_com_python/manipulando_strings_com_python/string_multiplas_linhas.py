db716806aea64b5e89b8ce6d46f7046ba061c82f
Douglas Rodrigues <dlorodriguesprogramador@gmail.com>
2025-05-06 21:37:09 -0300

    Create string_multiplas_linhas.py
    Strings de múltiplas linhas são definidas informando 3 aspas simples ou duplas.

---
+++ sintaxe_basica_com_python/manipulando_strings_com_python/string_multiplas_linhas.py
@@ -0,0 +1,21 @@
+# Introdução
+
+# Strings de múltiplas linhas são definidas informando 3 aspas simples ou duplas durante a atribuição. 
+# Elas podem ocupar várias linhas do código, e todos os espaços em branco são incluídos na string final.
+
+# Strings triplas
+
+nome = """Guilherme
+Arthur
+de
+Carvalho"""
+
+print(nome)
+
+print(
+    """
+    ===========================
+    ==   Bem-vindo ao jogo   ==
+    ===========================
+    """
+)
